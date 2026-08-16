a38d716c45812867e2ea7e71bd3943da3d7f7365
d1rtylegitness <1818kkoo111@gmail.com>
2026-08-16 22:45:41 +0300

    .

--- loader.lua
+++ loader.lua
@@ -25382,56 +25382,7 @@ end
 
 -- > ( finish loading )
 
-do
-    local username = "?"
-    local avatar = nil
-
-    local LRM_LinkedDiscordID = LRM_LinkedDiscordID or "320421345324433418"
-    if LRM_LinkedDiscordID then
-        local s, data = pcall(function()
-            local body = request({
-                ["Url"] = "https://discord-lookup-api-pied.vercel.app/v1/user/"..LRM_LinkedDiscordID,
-                ["Method"] = "GET",
-                ["Headers"] = {
-                    ["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-                    ["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-                    ["Accept-Language"] = "en-US,en;q=0.9",
-                    ["Connection"] = "keep-alive",
-                    ["Referer"] = "https://www.google.com/",
-                    ["DNT"] = "1",
-                    ["Upgrade-Insecure-Requests"] = "1"
-                }
-            })
-
-            if body and body["StatusCode"] == 200 then
-                local username = http_service:JSONDecode(body["Body"])
-
-                if username then
-                    return username
-                end
-            end
-        end)
-
-        username = (s and data) and "@"..data["username"] or "?"
-
-        if username ~= "?" then
-            s, avatar = pcall(function()
-                return game:HttpGet(data["avatar"]["link"])
-            end)
-        end
-    end
-
-    new_notification(
-        "welcome back to juju, "..username,
-        5,
-        color3_fromrgb(255, 255, 255),
-        avatar or base64_decode("iVBORw0KGgoAAAANSUhEUgAAAA4AAAAOCAYAAAAfSC3RAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAAYdEVYdFNvZnR3YXJlAFBhaW50Lk5FVCA1LjEuMvu8A7YAAAC2ZVhJZklJKgAIAAAABQAaAQUAAQAAAEoAAAAbAQUAAQAAAFIAAAAoAQMAAQAAAAIAAAAxAQIAEAAAAFoAAABphwQAAQAAAGoAAAAAAAAADHcBAOgDAAAMdwEA6AMAAFBhaW50Lk5FVCA1LjEuMgADAACQBwAEAAAAMDIzMAGgAwABAAAAAQAAAAWgBAABAAAAlAAAAAAAAAACAAEAAgAEAAAAUjk4AAIABwAEAAAAMDEwMAAAAADu6i0YxswAAgAAAGBJREFUOE+tkdEOgCAIRdH//+eCO3BG16TWeUEYB5zK7xyKHynNI9g1G01BRJZYDOjqjPpN3G2lG2cpGqIWOWUW89nwFHSPIG95DRNLwypNl6sapcmMqjiemAmPX/ANkROaKkTiEnqHHQAAAABJRU5ErkJggg==")
-    )
-
-    new_notification(
-        "please only use alternate accounts! read announcements for more info",
-        1
-    )
+new_notification("welcome back to juju", 5)
 
     local overlay = core_gui["RobloxPromptGui"]["promptOverlay"]
 
